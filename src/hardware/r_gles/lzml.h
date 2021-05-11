6f59018bbbbbd303aa6f89221bdf84ad97ef7db3
Jaime Ita Passos <jp6781615@gmail.com>
2021-05-11 00:02:58 -0300

    Update copyright year

--- src/hardware/r_gles/lzml.h
+++ src/hardware/r_gles/lzml.h
@@ -1,7 +1,7 @@
 /**
 	MIT License
 
-	Copyright (c) 2020 Jaime Passos
+	Copyright (c) 2020-2021 Jaime Ita Passos
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
